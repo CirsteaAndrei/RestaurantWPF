3e58dc1634933c0123c1f6849f5fcc20747f9657
CirsteaAndrei <andreicirstea2@gmail.com>
2023-09-04 12:02:53 +0300

    little update. Began working on Generate reports

--- Restaurant/Restaurant/ViewModels/AdminGenerateReportsVM.cs
+++ Restaurant/Restaurant/ViewModels/AdminGenerateReportsVM.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Restaurant.Models.Entities;
+using Restaurant.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Restaurant.Helpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Restaurant.ViewModels
 {
     class AdminGenerateReportsVM
     {
+        private UnitOfWork unitOfWork;
+        private ObservableCollection<User> usersList;
+
+        public AdminGenerateReportsVM() {
+
+            unitOfWork = ServiceLocator.ServiceProvider.GetService<UnitOfWork>();
+            usersList = unitOfWork.Users.ListToObsCollection(unitOfWork.Users.GetAllWithEmployeeData());
+        }
+
+        public ObservableCollection<User> UsersList
+        {
+            get
+            { return usersList; }
+            set { usersList = value; }
+        }
     }
 }
